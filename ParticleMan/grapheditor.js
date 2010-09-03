b51df90e00b85d09ac4c00190b655e385bd01840
Cody Brocious <cody.brocious@gmail.com>
2010-09-02 21:29:01 -0400

    Drag and drop, woo!

--- ParticleMan/grapheditor.js
+++ ParticleMan/grapheditor.js
@@ -1,29 +1,90 @@
 function graphEditor(id, width, height) {
 	sthis = this;
 	this.raphael = Raphael(id, width, height);
+	this.raphael.rect(0, 0, width, height).attr({
+			fill: '#eee',
+			stroke: '#222'
+		});
 	this.nodes = [];
-	
-	$(this.raphael.node).click(
-		function(event) {
-			sthis.addNode(event.clientX, event.clientY, new graphNode('hax', 'blah'));
-		}
-	);
+	this.selected = null;
 	
 	return true;
 }
 
 graphEditor.prototype.addNode = function(x, y, node) {
+	sthis = this;
 	this.nodes.push(node);
 	
-	circle = this.raphael.circle(x, y, 50);
-	circle.attr('fill', '#ccc');
-	circle.show();
-	this.raphael.safari();
+	node.parent = this;
+	node.focus(
+		function(node) {
+			if(sthis.selected != null)
+				sthis.selected.blur();
+			sthis.selected = node;
+			node.element.attr('stroke-width', 3);
+		}
+	);
+	node.blur(
+		function(node) {
+			sthis.selected = null;
+			node.element.attr('stroke-width', 1);
+		}
+	);
+	var set = node.element = this.raphael.set().push(
+		this.raphael.rect(x, y, 100, 50, 10).attr('fill', '#ccc'), 
+		this.raphael.text(x+20, y+20, node.body).attr('fill', '#000').attr('font-size', 20)
+	);
+	
+	set.click(
+		function() {
+			if(node.selected)
+				node.blur();
+			else
+				node.focus();
+		}
+	);
+	
+	function start() {
+		this.cx = this.cy = 0;
+		set.animate({'fill-opacity': 0.1}, 250);
+	}
+	function move(dx, dy) {
+		set.translate(dx - this.cx, dy - this.cy);
+		this.cx = dx;
+		this.cy = dy;
+		sthis.raphael.safari();
+	}
+	function end() {
+		set.animate({'fill-opacity': 1}, 250);
+	}
+	set.drag(move, start, end);
 }
 
 function graphNode(id, body) {
 	this.id = id;
 	this.body = body;
 	
+	this.focusHooks = [];
+	this.blurHooks = [];
+	this.selected = false;
+	
 	return true;
+}
+
+graphNode.prototype.focus = function(hook) {
+	if(hook == undefined) {
+		this.selected = true;
+		for(hook in this.focusHooks)
+			this.focusHooks[hook](this);
+	} else
+		this.focusHooks.push(hook);
+}
+
+graphNode.prototype.blur = function(hook) {
+	if(hook == undefined) {
+		this.selected = false;
+		for(hook in this.blurHooks)
+			this.blurHooks[hook](this);
+	} else
+		this.blurHooks.push(hook);
 }
--- ParticleMan/particleman.js
+++ ParticleMan/particleman.js
@@ -1,6 +1,7 @@
 var graph;
 
 function ready() {
-	graph = new graphEditor('graph-editor', 320, 240);
+	graph = new graphEditor('graph-editor', 640, 300);
 	graph.addNode(100, 100, new graphNode('foo', 'bar'));
+	graph.addNode(200, 200, new graphNode('baz', 'hax'));
 }
