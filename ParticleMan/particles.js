7f75d7c8a42c0116de7443e569130cf4f34194ad
Cody Brocious <cody.brocious@gmail.com>
2010-09-03 06:59:08 -0400

    Attractors.

--- ParticleMan/particles.js
+++ ParticleMan/particles.js
@@ -1,4 +1,10 @@
+var interval;
+function stop() {
+	clearInterval(interval);
+}
+
 function emitter(x, y, speed, frequency, lifetime) {
+	this.type = emitter;
 	this.x = x;
 	this.y = y;
 	this.speed = speed;
@@ -14,7 +20,41 @@ emitter.prototype.update = function() {
 	this.parent.add(new particle(this.x, this.y, dx, dy, this.lifetime));
 };
 
+function attractor(x, y, gravity) {
+	this.type = attractor;
+	this.x = x;
+	this.y = y;
+	this.gravity = gravity;
+	for(name in this)
+		console.log(name);
+}
+
+attractor.prototype.update = function() {
+	var sthis = this;
+	this.parent.each(
+		function(elem) {
+			if(elem.type != particle) return;
+			
+			var dx = elem.x - sthis.x;
+			var dy = elem.y - sthis.y;
+			var power = sthis.gravity / Math.sqrt(dx*dx + dy*dy);
+			
+			elem.x -= dx * power;
+			elem.y -= dy * power;
+			if(elem.x < sthis.x || elem.sx > 0)
+				elem.sx += power;
+			else
+				elem.sx -= power;
+			if(elem.x < sthis.y || elem.sy > 0)
+				elem.sy += power;
+			else
+				elem.sy -= power;
+		}
+	);
+};
+
 function particle(x, y, sx, sy, lifetime) {
+	this.type = particle;
 	this.x = x;
 	this.y = y;
 	this.sx = sx;
@@ -26,15 +66,13 @@ particle.prototype.update = function() {
 	this.x += this.sx;
 	this.y += this.sy;
 	if(this.lifetime-- == 0) {
-		console.log(this.parent.elements.length);
 		this.parent.remove(this);
-		console.log(this.parent.elements.length);
 	}
 };
 
 particle.prototype.draw = function() {
 	this.parent.ctx.beginPath();
-	this.parent.ctx.arc(this.x, this.y, this.lifetime / 2, 0, Math.PI*2, true);
+	this.parent.ctx.arc(this.x, this.y, Math.sqrt(this.lifetime), 0, Math.PI*2, true);
 	this.parent.ctx.fill();
 };
 
@@ -46,11 +84,12 @@ function particleSystem(id) {
 	this.removed = false;
 	this.elements = [];
 	
-	this.add(new emitter(200, 200, 2, 1, 50));
-	this.add(new emitter(400, 400, 5, 1, 20));
+	this.add(new emitter(100, 100, 2, 1, 100));
+	this.add(new attractor(400, 200, 4));
+	this.add(new emitter(500, 400, 5, 1, 100));
 	
 	var sthis = this;
-	setInterval(function() { sthis.loop() }, 1000 / 60);
+	interval = setInterval(function() { sthis.loop() }, 1000 / 60);
 }
 
 particleSystem.prototype.add = function(element) {
